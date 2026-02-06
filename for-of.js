642f0a03ecfb8e01882f2af7d2e427f242790cff
Ittehad Siddiquee <ittehad006@gmail.com>
2026-02-06 22:31:16 +0600

    added

---
+++ for-of.js
@@ -0,0 +1,12 @@
+const numbers = [12,45,62,56,67,78];
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+
+}
+const fruits = ['orange','apple', 'mango','jackfruit' ,'watermelon']
+for (const fruit of fruits){
+ console.log(fruit);
+ console.log('i want to eat');
+}
---
+++ sum.js
@@ -0,0 +1,9 @@
+let num = 1;
+let sum = 00;
+
+while (num <=10){
+    console.log(num);
+    console.log('sum',sum);
+    num++;
+
+}
