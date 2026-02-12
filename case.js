d1a7fd8caa4a25c105d5c44c29c3e970e66d1836
Ittehad Siddiquee <ittehad006@gmail.com>
2026-02-12 17:27:50 +0600

    string

---
+++ case.js
@@ -0,0 +1,26 @@
+const school = 'sitakund high school';
+console.log(school); 
+console.log(school.toLowerCase()); 
+console.log(school.toUpperCase()); 
+
+
+const subject = 'chemistry'
+const book = 'chemistry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('i am reading book');
+}
+else{
+    console.log('hudai hudai vat khai')
+}
+
+const drink = 'water';
+const liquid = '  water';
+
+if(drink===liquid){
+    console.log('pani ar upor nam life');
+
+}
+else{
+    console.log('somudre pani ase khaite parii nai');
+}
---
+++ slice.js
@@ -0,0 +1,17 @@
+const address = 'andorkilla';
+const part = address.slice(2,5);
+console.log(part);
+
+const sentence = 'I am a good and harworking person.';
+console.log(sentence.split(''));
+console.log(sentence.split('a'));
+
+
+const friendStr = 'rahim,akila,lahim,dalim,ifty,sabu';
+const friends = friendsStr.split(',');
+console.log(friends);
+
+const realfriend = ['rahim','akila','lahim','dalim','ifty','sabu'];
+console.log(realFriend.join());
+console.log(realFriend.join('|'));
+console.log(realFriend.join('-'));
