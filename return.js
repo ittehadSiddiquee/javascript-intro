744e3e0545c1bd9a2601db7fe66b6f0c05e16b21
Ittehad Siddiquee <ittehad006@gmail.com>
2026-02-16 21:21:49 +0600

    function

---
+++ return.js
@@ -0,0 +1,17 @@
+function TenTimes (number){
+    const result = number * 10;
+    return result;
+    console.log(result);
+}
+
+fucntion cuthalf(number){
+     const half = number / 2 ;
+     return half;
+}
+
+TenTimes(5);
+
+const output = TenTimes(5);
+console.log('output',output);
+
+const bignumber = TenTimes(99);
